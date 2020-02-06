a76f76483b9101d1704d1acddf66df5dbf7c467a
ERICK MATHEUS RODRIGUES SOUSA <19266837@CFP127ALUNO.LOCAL>
2020-02-06 11:25:18 -0300

    refatorando e utilizando arrow function

--- aula_01/script.js
+++ aula_01/script.js
@@ -2,15 +2,11 @@
 
 const $calc = document.getElementById( 'calc' );
 
-function calcMedia () {
-    const $nome = document.getElementById( 'nome' );
-    const $nota1 = document.getElementById( 'nota1' );
-    const $nota2 = document.getElementById( 'nota2' );
-    const $media = document.getElementById( 'media' );    
-    const $situacao = document.getElementById( 'situacao' );    
+const calcMedia = (n1 , n2) => (parseInt(n1) + parseInt(n2)) / 2;
 
-    const media = (parseInt($nota1.value)+parseInt($nota2.value))/2;
+const calcSituacao = ( media ) => media >=
 
+/*{
     if ( media >= 50 ){
         $situacao.value="Aprovado";
         $situacao.classList.remove('reprovado');
@@ -21,11 +17,47 @@ function calcMedia () {
         $situacao.classList.remove('aprovado');
         $situacao.classList.add('reprovado');
         
-    }
-
-    $media.value=media;
+    }*/
 
     console.log($nome.value);
 }
 
-$calc.addEventListener('click', calcMedia);
+const exibirMedia = () => {
+    const $nome = document.getElementById( 'nome' );
+    const nota1 = document.getElementById( 'nota1' ).value;
+    const nota2 = document.getElementById( 'nota2' ).value;
+    const $media = document.getElementById( 'media' );    
+    const $situacao = document.getElementById( 'situacao' );
+
+    const media = calcMedia (nota1 , nota2);
+
+    $media.value = media;
+}
+
+const calcConceito = () => {
+    const $conceito = document.getElementById( 'conceito' );   
+    const media = document.getElementById( 'media' ).value;
+    
+    if(media < 0 || media > 100){
+        $conceito.value= "*ERRO*";
+    }else if (media < 30){
+        $conceito.value= "E";
+    }else if(media < 50){
+        $conceito.value= "D";
+    }else if(media < 80){
+        $conceito.value= "C";
+    }else if(media < 100){
+        $conceito.value= "B";
+    }else if(media == 100){
+        $conceito.value= "A";
+    }
+
+}
+
+const calcular = () =>{
+    calcSituacao();
+    calcConceito();
+    exibirMedia();
+}
+
+$calc.addEventListener('click', calcular);
