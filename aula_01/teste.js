f8ce4ce5440568f44687f0643cf5e2094d48b091
ERICK MATHEUS RODRIGUES SOUSA <19266837@CFP127ALUNO.LOCAL>
2020-02-06 08:15:35 -0300

    Meu primeiro commit

---
+++ aula_01/teste.js
@@ -0,0 +1,28 @@
+const nome="Erick";
+const idade=12;
+const teste=true;
+const prduto={produto: "mouse", quantidade: "45"};
+const somar=() => 3 + 3;
+
+console.log( typeof ( nome ) );
+console.log( typeof ( idade ) );
+console.log( typeof ( teste ) );
+console.log( typeof ( produto ) );
+console.log( typeof ( somar ) );
+
+const cidade="Itapevi";
+const estado='Sao Paulo';
+const pais='Brasil ${estado}';
+
+console.log("Cidade: ", cidade);
+console.log("Estado: ", estado);
+console.log("Pais: ", pais);
+
+let nota1=10;
+let nota2=10;
+
+console.log(nota1++);
+console.log(++nota2);
+
+console.log(nota1++);
+console.log(++nota2);
